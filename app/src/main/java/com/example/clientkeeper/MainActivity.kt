e66b53370feddecf089773566c6564bc17d2c3d7
TharushiSamindara <tharushisamindara@gmail.com>
2024-07-23 16:10:03 +0530

    Welcome Page code

--- app/src/main/java/com/example/clientkeeper/MainActivity.kt
+++ app/src/main/java/com/example/clientkeeper/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.clientkeeper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Disappear this page after 1s
+        GlobalScope.launch {
+            delay(1000) // Delay for 1 second (1000 milliseconds)
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
