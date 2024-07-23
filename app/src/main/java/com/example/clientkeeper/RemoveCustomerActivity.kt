3b03d13c40f27658ef9063f0c68f5f8934982bb7
TharushiSamindara <tharushisamindara@gmail.com>
2024-07-23 09:13:20 +0530

    Remove Customer Design

---
+++ app/src/main/java/com/example/clientkeeper/RemoveCustomerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.clientkeeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RemoveCustomerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_remove_customer)
+    }
+}
