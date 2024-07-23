36366d43ab4ea71912b40cda243a3e81b53b6db0
TharushiSamindara <tharushisamindara@gmail.com>
2024-07-23 12:53:31 +0530

    Edit Customer Design

---
+++ app/src/main/java/com/example/clientkeeper/EditCustomerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.clientkeeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditCustomerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_customer)
+    }
+}
