d3df23d6d6acc25ad8b08972a619871c117fcf90
TharushiSamindara <tharushisamindara@gmail.com>
2024-07-24 21:24:21 +0530

    new user register to the system

--- app/src/main/java/com/example/clientkeeper/LoginActivity.kt
+++ app/src/main/java/com/example/clientkeeper/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.example.clientkeeper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var helper = DBHelper(applicationContext)
+
+        var username= findViewById<EditText>(R.id.editTextUsername)
+        var password= findViewById<EditText>(R.id.editTextPassword)
+        var btnLogin = findViewById<Button>(R.id.btnLogin)
+        var btnSignIn = findViewById<Button>(R.id.btnSignIn)
+
+        //go to register (for new user)
+        btnSignIn.setOnClickListener{
+            val gotoNextScreen = Intent(applicationContext,SignInActivity::class.java)
+            startActivity(gotoNextScreen)
+        }
     }
 }
--- app/src/main/java/com/example/clientkeeper/SignInActivity.kt
+++ app/src/main/java/com/example/clientkeeper/SignInActivity.kt
@@ -1,11 +1,40 @@
 package com.example.clientkeeper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        var helper = DBHelper(applicationContext)
+
+        var username= findViewById<EditText>(R.id.editTextUsername)
+        var password= findViewById<EditText>(R.id.editTextPassword)
+        var btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        var indexqlobal = 0
+
+        btnLogin.setOnClickListener{
+            var index = ++indexqlobal
+
+            helper.insertData1(
+                index.toString(),
+                username.text?.toString()!!,
+                password.text?.toString()!!
+            )
+
+            val usernameVal = index.toString()
+            val bundle = Bundle()
+            bundle.putString("usernamePara", usernameVal)
+
+            val gotoNextScreen = Intent(applicationContext,DashboardActivity::class.java)
+            gotoNextScreen.putExtras(bundle)
+            startActivity(gotoNextScreen)
+        }
     }
 }
