2651fee284ad7725e16fdf4138dd8751d88a8239
TharushiSamindara <tharushisamindara@gmail.com>
2024-07-26 16:21:19 +0530

    give access to other crud activities from dashboard

--- app/src/main/java/com/example/clientkeeper/DashboardActivity.kt
+++ app/src/main/java/com/example/clientkeeper/DashboardActivity.kt
@@ -1,13 +1,15 @@
 package com.example.clientkeeper
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class DashboardActivity : AppCompatActivity() {
-    @SuppressLint("Recycle")
+    @SuppressLint("Recycle", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
@@ -19,5 +21,42 @@ class DashboardActivity : AppCompatActivity() {
 
         val usernameInsert = findViewById<TextView>(R.id.txtDashboardUsername)
         usernameInsert.text = usernameFromLogin
+
+        val btnSearch = findViewById<Button>(R.id.btnSearch)
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        val btnRemove = findViewById<Button>(R.id.btnRemove)
+        val btnEdit = findViewById<Button>(R.id.btnEdit)
+
+        //bundle
+        val bundle = Bundle()
+        bundle.putString("usernamePara", usernameFromLogin)
+
+        //go to search a customer page
+        btnSearch.setOnClickListener{
+            val gotoNextScreen = Intent(applicationContext,SearchCustomerActivity::class.java)
+            gotoNextScreen.putExtras(bundle)
+            startActivity(gotoNextScreen)
+        }
+
+        //go to add a customer page
+        btnAdd.setOnClickListener{
+            val gotoNextScreen = Intent(applicationContext,NewCustomerActivity::class.java)
+            gotoNextScreen.putExtras(bundle)
+            startActivity(gotoNextScreen)
+        }
+
+        //go to remove a customer page
+        btnRemove.setOnClickListener{
+            val gotoNextScreen = Intent(applicationContext,RemoveCustomerActivity::class.java)
+            gotoNextScreen.putExtras(bundle)
+            startActivity(gotoNextScreen)
+        }
+
+        //go to edit a piece of customer information page
+        btnEdit.setOnClickListener{
+            val gotoNextScreen = Intent(applicationContext,EditCustomerActivity::class.java)
+            gotoNextScreen.putExtras(bundle)
+            startActivity(gotoNextScreen)
+        }
     }
 }
