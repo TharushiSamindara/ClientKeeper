7ccbcf8e75cf69f99bc9498dd29faabe8d526887
TharushiSamindara <tharushisamindara@gmail.com>
2024-07-28 19:31:11 +0530

    edit text function

--- app/src/main/java/com/example/clientkeeper/EditCustomerActivity.kt
+++ app/src/main/java/com/example/clientkeeper/EditCustomerActivity.kt
@@ -1,12 +1,18 @@
 package com.example.clientkeeper
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 
 class EditCustomerActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_customer)
@@ -17,6 +23,12 @@ class EditCustomerActivity : AppCompatActivity() {
 
         val usernameInsert = findViewById<TextView>(R.id.txtUsername)
         usernameInsert.text = usernameFromLogin
+
+        //get column name
+        val editColumn = findViewById<RadioGroup>(R.id.radioGroup)
+
+        //get edited customer index
+        val editedCustomerIndex = findViewById<EditText>(R.id.editTextEditedIndex)
 
         //back button
         val btnBack = findViewById<Button>(R.id.btnBack)
